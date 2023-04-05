11af32919f11c2ec1e3bfd1a219fe82620305a40
PeaceUCR <peace940814202@gmail.com>
2023-04-05 23:19:15 +0800

    try fix the

--- src/main/java/com/example/sofabackend/domain/User.java
+++ src/main/java/com/example/sofabackend/domain/User.java
@@ -1,8 +1,11 @@
 package com.example.sofabackend.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -21,5 +24,9 @@ public class User {
     private Integer score;
     private String nickname;
     private String avatarUrl;
+
+    // TODO correct the lastTimeLogin, it shouldn't be a array
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime lastTimeLogin;
 }
