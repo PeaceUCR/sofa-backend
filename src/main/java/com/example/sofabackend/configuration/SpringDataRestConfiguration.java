09b6c65e30232f7aa385b585514075cea98effee
peace <peace940814202@gmail.com>
2023-04-05 23:40:12 +0800

    disable spring data rest

---
+++ src/main/java/com/example/sofabackend/configuration/SpringDataRestConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.sofabackend.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class SpringDataRestConfiguration implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.disableDefaultExposure();
+    }
+}
