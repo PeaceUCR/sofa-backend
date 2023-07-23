0b8fbce17993c8b0d12c930ef025f5c5ba994bcb
PingHe <peace940814202@gmail.com>
2023-07-23 21:51:02 +0800

    try fix the LocalDateTime parse

--- src/main/java/com/example/sofabackend/domain/UserCollection.java
+++ src/main/java/com/example/sofabackend/domain/UserCollection.java
@@ -1,11 +1,14 @@
 package com.example.sofabackend.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -24,5 +27,8 @@ public class UserCollection {
     private String collectionId;
     private String type;
     private String title;
+    // TODO correct the lastTimeLogin, it shouldn't be a array
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime time;
 }
